d07b077bafac09dee10c59579cd6fb88bd0f4544
Selim <selim.jouini@esprit.tn>
2026-09-06 01:02:19 +0100

    Update app message to test full pipeline

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+﻿from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"Hello from the pipeline!")
+        self.wfile.write(b"Hello from Nyxion - pipeline v2!")
 
 HTTPServer(('0.0.0.0', 8080), Handler).serve_forever()
